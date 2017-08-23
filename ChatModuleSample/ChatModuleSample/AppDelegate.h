3db9e08e58b249ac698b0cbce8a35deab43f618b
Ginger <ginger.jin@juphoon.com.cn>
2017-08-23 11:48:13 +0800

    init commit

---
+++ ChatModuleSample/ChatModuleSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChatModuleSample
+//
+//  Created by Ginger on 2017/8/23.
+//  Copyright © 2017年 Ginger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChatModuleSample/ChatModuleSample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ChatModuleSample
+//
+//  Created by Ginger on 2017/8/23.
+//  Copyright © 2017年 Ginger. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
